3aa71d11ab9c8753f7bc445107943586230c7c98
Apisara Ngakor <apisara.99@mail.kmutt.ac.th>
2021-04-19 22:20:20 +0700

    [UPDATE]62130500103 interface stock.vue

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,94 +1,94 @@
 <template >
   <navbar />
   <!-- <header class="flex justify-center">Stock Manager</header> -->
-
-  <body class="">
+  <div>
     <div class="contianer justify-center flex mt-20">
       <div
-        class="stock-contianer justify-start max-w-lg rounded-md shadow-lg bg-red-100 px-8 py-10"
+        class="stock-contianer justify-start max-w-lg rounded-md shadow-lg px-8 py-10"
+        style="background-color: #ffddd2"
       >
-      <form @submit.prevent="submitForm">
-        <div class="mb-8">
-          <p>
-            Item Name :
-            <input
-              v-model="enteredName"
-              class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
-              type="text"
-              placeholder="Plese enter your item name"
-            />
-          </p>
-        </div>
+        <form @submit.prevent="submitForm">
+          <div class="mb-8">
+            <p>
+              Item Name :
+              <input
+                v-model="enteredName"
+                class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
+                type="text"
+                placeholder="Enter Item name"
+              />
+            </p>
+          </div>
 
-        <div class="mb-8">
-          <p>
-            Price :
-            <input
-              v-model="price"
-              class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
-              type="text"
-              placeholder="Item price"
-            />
-          </p>
-        </div>
+          <div class="mb-8">
+            <p>
+              Price :
+              <input
+                v-model="price"
+                class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
+                type="text"
+                placeholder="Item price"
+              />
+            </p>
+          </div>
 
-        <div class="mb-8">
-          <p>
-            Quantity :
-            <input
-              v-model="quantity"
-              class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
-              type="text"
-              placeholder="Item quantities"
-            />
-          </p>
-        </div>
+          <div class="mb-8">
+            <p>
+              Quantity :
+              <input
+                v-model="quantity"
+                class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
+                type="text"
+                placeholder="Item quantities"
+              />
+            </p>
+          </div>
 
-        <div class="mb-8">
-          <p>
-            Description :
-            <input
-              v-model="description"
-              class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
-              type="text"
-              placeholder="Description"
-            />
-          </p>
-        </div>
-       </form> 
+          <div class="mb-8">
+            <p>
+              Description :
+              <input
+                v-model="description"
+                class="ml-2 w-80 placeholder-gray-500 placeholder-opacity-50 focus:outline-none rounded"
+                type="text"
+                placeholder="Description"
+              />
+            </p>
+          </div>
+        </form>
       </div>
     </div>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center my-20">
+      <div class="inline">
         <button
-        @click="addProduct"
-        class="rounded-md py-2 px-5 my-8"
-        style="background-color: #dd2c2f">
-        <!-- <router-link to="/stock"> -->
-        SUBMIT
-        <!-- </router-link> -->
-      </button>
-      <!-- <button
-        @click="addProduct"
-        class="rounded-md py-2 px-5 my-8"
-        style="background-color: #dd2c2f">
-        <router-link to="/stock">
-        Go to Stock!
-        </router-link>
-      </button> -->
+          @click="addProduct"
+          class="rounded-md py-2 px-5 mx-16"
+          style="background-color: #dd2c2f"
+        >
+          SUBMIT
+        </button>
+      </div>
+      <div class="inline">
+        <button
+          class="rounded-md py-4 px-5"
+          style="background-color: #2ba84a"
+        >
+          <router-link to="/stock"> Go to Stock! </router-link>
+        </button>
+      </div>
     </div>
-  </body>
+  </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue';
+import Navbar from "../components/Navbar.vue";
 // @ is an alias to /src
 
 export default {
   name: "Home",
   components: {
-    Navbar
-    
+    Navbar,
   },
 
   data() {
@@ -114,7 +114,7 @@ export default {
       this.invaliQuantityInput = this.quantity === null ? true : false;
       this.invaliDescription = this.description === "" ? true : false;
 
-      this.addProduct()
+      this.addProduct();
 
       // console.log(`name value: ${this.enteredName}`);
       // console.log(`price value: ${this.price}`);
@@ -150,15 +150,15 @@ export default {
             quantity: this.quantity,
             description: this.description,
           }),
-        })
-        
+        });
+
         const data = await res.json();
         this.allProduct = [...this.allProduct, data];
         this.enteredName = "";
         this.price = null;
         this.quantity = null;
         this.description = "";
-        } catch (error) {
+      } catch (error) {
         console.log(`Could not save! ${error}`);
       }
     },
@@ -179,10 +179,8 @@ export default {
         console.log(`Could not get! ${error}`);
       }
     },
-
-    
   },
-}
+};
 </script>
 
 <style scoped>
@@ -192,19 +190,19 @@ header {
   color: #006d77;
   padding: 24px;
 }
-body {
-  color: #006d77;
-}
 
 input {
   background-color: #ffffff;
-  color: #e29578;
+  color: #006d77;
   padding: 1px;
 }
 
 button {
   color: #ffffff;
   padding: 4px;
+}
+div {
+  color: #006d77;
 }
 </style>
 
